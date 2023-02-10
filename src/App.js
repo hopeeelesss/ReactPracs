3a9ca8deeea06223ac62302b5b2d89daf2b47e5f
Victor <khokhlov34@gmail.com>
2023-02-10 16:54:51 +0300

    Prac 1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const element = "whouser"
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome, {element}</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
